417c03aa454c9871d414e7a4e7ed94011d2d4a6b
Ayse Stinnett <aysestinnett@Ayses-MBP.attlocal.net>
2021-11-15 12:41:18 -0500

    Initial commit from gatsby

---
+++ gatsby-config.js
@@ -0,0 +1,7 @@
+module.exports = {
+  siteMetadata: {
+    siteUrl: "https://www.yourdomain.tld",
+    title: "ayseboogie",
+  },
+  plugins: ["gatsby-plugin-react-helmet", "gatsby-plugin-sitemap"],
+};
