41f077d7b9087f0b5e2e1c7e395761661a621260
Ayse Stinnett <aysestinnett@Ayses-MBP.home>
2021-11-18 21:05:26 -0500

    mobile vs desktop header visibility

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -8,11 +8,15 @@ const Header = ({ pageTitle }) => {
   return (
     <div className="w-full" style={{ display: "grid" }}>
       {/* You can use a GatsbyImage component if the image is dynamic */}
+      {/* mobile header */}
       <StaticImage
+        className="md:invisible"
+        loading="eager"
+        objectFit="contain"
         style={{
           gridArea: "1/1",
           // You can set a maximum height for the image, if you wish.
-          maxHeight: "375px",
+          maxHeight: "425px",
         }}
         layout="fullWidth"
         // You can optionally force an aspect ratio for the generated image
@@ -20,7 +24,26 @@ const Header = ({ pageTitle }) => {
         // This is a presentational image, so the alt should be an empty string
         alt=""
         // Assisi, Perúgia, Itália by Bernardo Ferrari, via Unsplash
-        src={"../../images/bermuda.jpeg"}
+        src={"../../images/bike.jpeg"}
+        formats={["auto", "webp", "avif"]}
+      />
+      {/*  desktp header */}
+      <StaticImage
+        className="invisible md:visible"
+        loading="eager"
+        // objectFit="contain"
+        style={{
+          gridArea: "1/1",
+          // You can set a maximum height for the image, if you wish.
+          maxHeight: "425px",
+        }}
+        layout="fullWidth"
+        // You can optionally force an aspect ratio for the generated image
+        aspectRatio={3 / 1}
+        // This is a presentational image, so the alt should be an empty string
+        alt=""
+        // Assisi, Perúgia, Itália by Bernardo Ferrari, via Unsplash
+        src={"../../images/bridge.jpeg"}
         formats={["auto", "webp", "avif"]}
       />
 
@@ -38,7 +61,7 @@ const Header = ({ pageTitle }) => {
       >
         <div className={cn("w-full text-center text-white", style.textBanner)}>
           <nav>
-            <ul className="flex list-none justify-end tracking-widest text-black">
+            <ul className="flex list-none justify-end tracking-widest text-white md:text-lg">
               <li className="pr-8">
                 <AnchorLink to="/" title="Home">
                   <span>Home</span>
