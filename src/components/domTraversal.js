715645d0824432c0fcb1e55e01567da5ea3f92a5
Ayse Stinnett <aysestinnett@Ayses-MBP.home>
2021-11-30 02:36:08 -0500

    two traverse buttons

--- src/components/domTraversal.js
+++ src/components/domTraversal.js
@@ -5,15 +5,14 @@ export class DomTraversal extends Component {
   constructor(props) {
     super(props);
 
-    this.state = {
-      // expandedId: false,
-      // // isVisible: false,
-      // offsetTop: 0,
-    };
+    this.state = {};
   }
 
   buttClick() {
     const html = document.documentElement;
+
+    console.log("0 ", html);
+
     const h1 = html.getElementsByTagName("h1")[0];
     console.log("2 ", h1);
     const p = document.getElementsByTagName("p")[0];
@@ -26,18 +25,90 @@ export class DomTraversal extends Component {
   }
 
   render() {
+    const tree = {
+      id: 1,
+      label: "A",
+      child: [
+        {
+          id: 2,
+          label: "B",
+          child: [
+            { id: 5, label: "E", child: [] },
+            { id: 6, label: "F", child: [] },
+            {
+              id: 7,
+              label: "G",
+              child: [],
+            },
+          ],
+        },
+        { id: 3, label: "C", child: [] },
+        {
+          id: 4,
+          label: "D",
+          child: [
+            { id: 8, label: "H", child: [] },
+            { id: 9, label: "I", child: [] },
+          ],
+        },
+      ],
+    };
+
+    const search = (tree, target) => {
+      if (tree.id === target) {
+        // return
+        console.log("tree label", tree.label);
+      }
+      for (const child of tree.child) {
+        const res = search(child, target);
+
+        if (res) {
+          // return res;
+          console.log("res", res);
+        }
+      }
+    };
+
     return (
       <html lang="en">
         <head>
           <title>Learning About Nodes</title>
         </head>
 
-        <body>
-          <button type="submit" onClick={this.buttClick}>
+        <body style={{ textAlign: "center" }}>
+          <button
+            type="button"
+            style={{
+              overflow: "hidden",
+              padding: 10,
+              cursor: "pointer",
+              borderRadius: 25,
+              backgroundColor: "#B76E79",
+              letterSpacing: 0.1 + "em",
+            }}
+            onClick={search(tree, 7)}
+          >
+            test
+          </button>
+
+          <br />
+          <br />
+          <button
+            type="button"
+            style={{
+              overflow: "hidden",
+              padding: 10,
+              cursor: "pointer",
+              borderRadius: 25,
+              backgroundColor: "#B76E79",
+              letterSpacing: 0.1 + "em",
+            }}
+            onClick={this.buttClick}
+          >
             Refresh Page
           </button>
           <br />
-          <h1>Shark World</h1>
+          <h1 style={{ color: "red" }}>Shark World</h1>
           <h2 id="app">app</h2>
           <p>
             The world's leading source on <strong>shark</strong> related
--- src/components/projectsContainer/projectsContainer.js
+++ src/components/projectsContainer/projectsContainer.js
@@ -8,6 +8,7 @@ const ProjectsContainer = () => {
   return (
     <div className="w-full bg-gray-800 text-2xl tracking-widest pb-10 py-8 md:py-16">
       <div className="flex flex-wrap justify-center text-center lg:px-32">
+        {/* 0 */}
         <div
           className={cn(
             "relative w-3/4 md:w-1/3 mx-3",
@@ -45,6 +46,7 @@ const ProjectsContainer = () => {
           </Link>
         </div>
 
+        {/* 1 */}
         <div
           className={cn(
             "relative w-3/4 md:w-1/3 mx-3",
@@ -68,6 +70,47 @@ const ProjectsContainer = () => {
           />
           <Link
             to="/experiences"
+            className={cn("opacity-0 absolute text-center", style.projectsText)}
+            style={{
+              transition: ".5 ease",
+              top: "27%",
+              left: "50%",
+              transform: "translate(-50%, -50%)",
+            }}
+          >
+            <div className="text-white text-sm invisible lg:visible md:text-lg lg:text-2xl md:my-16">
+              Lonely Planet Experiences
+            </div>
+            <div className="text-white text-sm md:text-base lg:text-xl underline">
+              details
+            </div>
+          </Link>
+        </div>
+
+        {/* 2 */}
+        <div
+          className={cn(
+            "relative w-3/4 md:w-1/3 mx-3",
+            style.projectsContainer
+          )}
+        >
+          <Link
+            to="/experiences"
+            className="text-white whitespace-nowrap lg:hidden text-sm"
+          >
+            DOM Traversal
+          </Link>
+          <StaticImage
+            src="../../images/lpExperiences.png"
+            alt="LP Experiences"
+            className={cn("w-full opacity-100 block rounded-md", style.image)}
+            style={{
+              transition: ".5s ease",
+              backfaceVisibility: "hidden",
+            }}
+          />
+          <Link
+            to="/dom"
             className={cn("opacity-0 absolute text-center", style.projectsText)}
             style={{
               transition: ".5 ease",
