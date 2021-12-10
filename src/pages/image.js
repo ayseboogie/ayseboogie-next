59de7e38d7e16795180480cdda619a40058d6b54
Ayse Stinnett <aysestinnett@Ayses-MBP.home>
2021-12-09 21:40:56 -0500

    hosting own image to try to use as og:image

---
+++ src/pages/image.js
@@ -0,0 +1,8 @@
+import * as React from "react";
+import { StaticImage } from "gatsby-plugin-image";
+
+const Image = () => {
+  return <StaticImage src="../images/ayseSite.png" alt="Ayse" />;
+};
+
+export default Image;
