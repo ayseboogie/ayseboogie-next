9cb6eab6c0b63ac76c31166bb0ea93787e7207f7
Ayse Stinnett <aysestinnett@Ayses-MBP.home>
2021-12-14 01:36:43 -0500

    relying on og for select twitter fields

--- src/pages/index.js
+++ src/pages/index.js
@@ -18,14 +18,14 @@ const IndexPage = () => {
         <link rel="canonical" href="http://ayse.site" />
         <link rel="icon" href={Triangle} />
         <meta property="og:image" content={MI} />
-        <meta property="twitter:card" content="summary_large_image" />
-        <meta property="twitter:site" content="@AyseStinnett" />
-        <meta property="twitter:title" content="Ayse Site" />
-        <meta
-          property="twitter:description"
-          content="I am a frontend engineer with a passion for web development."
-        />
-        <meta property="twitter:image" content={MI} />
+        {/*<meta name="twitter:card" content="summary_large_image" />*/}
+        {/*<meta name="twitter:creator" content="@AyseStinnett" />*/}
+        {/*<meta name="twitter:title" content="Ayse Site" />*/}
+        {/*<meta*/}
+        {/*  name="twitter:description"*/}
+        {/*  content="I am a frontend engineer with a passion for web development."*/}
+        {/*/>*/}
+        {/*<meta name="twitter:image" content={MI} />*/}
       </Helmet>
       <Layout pageTitle="Home">
         <Seo lang="en" title="Home" />
