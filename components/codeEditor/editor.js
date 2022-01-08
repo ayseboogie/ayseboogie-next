9267ab24de5e9f114c976650b1e989364fb8a201
Ayse Stinnett <aysestinnett@Ayses-MBP.home>
2022-01-07 22:41:38 -0500

    5

--- components/codeEditor/editor.js
+++ components/codeEditor/editor.js
@@ -1,5 +1,6 @@
 import React from "react";
 // import loadable from "@loadable/component";
+import dynamic from "next/dynamic";
 import "codemirror/lib/codemirror.css";
 import "codemirror/theme/material.css";
 import "codemirror/mode/xml/xml";
@@ -8,16 +9,19 @@ import "codemirror/mode/css/css";
 // import { Controlled as ControlledEditor } from "react-codemirror2-react-17";
 import * as style from "./editorApp.module.css";
 
+const CodeMirror = dynamic(() => import("react-codemirror2-react-17"), {
+  ssr: false,
+});
 // const { ControlledEditor } = loadable(() =>
 //   import("react-codemirror2-react-17")
 // );
 
-let CodeMirror = null;
-if (typeof window !== "undefined" && typeof window.navigator !== "undefined") {
-  CodeMirror = require("react-codemirror2-react-17");
-  require("codemirror/mode/yaml/yaml");
-  require("codemirror/mode/dockerfile/dockerfile");
-}
+// let CodeMirror = null;
+// if (typeof window !== "undefined" && typeof window.navigator !== "undefined") {
+//   CodeMirror = require("react-codemirror2-react-17");
+//   require("codemirror/mode/yaml/yaml");
+//   require("codemirror/mode/dockerfile/dockerfile");
+// }
 
 export default function Editor(props) {
   const { language, displayName, value, onChange } = props;
