88b31aeb524a19664d6f0ec0065f7a550cfa8c2b
Ayse Stinnett <aysestinnett@Ayses-MacBook-Pro.local>
2022-01-08 18:12:25 -0500

    this works...

--- components/codeEditor/editor.js
+++ components/codeEditor/editor.js
@@ -1,37 +1,26 @@
-import React, { useState } from "react";
+import React from "react";
 import "codemirror/lib/codemirror.css";
 import "codemirror/theme/material.css";
 // import "codemirror/mode/xml/xml";
 // import "codemirror/mode/javascript/javascript";
 // import "codemirror/mode/css/css";
 import { Controlled as ControlledEditor } from "react-codemirror2-react-17";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faCompressAlt, faExpandAlt } from "@fortawesome/free-solid-svg-icons";
+import * as style from "./editorApp.module.css";
 
 export default function Editor(props) {
   const { language, displayName, value, onChange } = props;
-  const [open, setOpen] = useState(true);
 
   function handleChange(editor, data, value) {
     onChange(value);
   }
 
   return (
-    <div className={`editor-container ${open ? "" : "collapsed"}`}>
-      <div className="editor-title">
-        {displayName}
-        <button
-          type="button"
-          className="expand-collapse-btn"
-          onClick={() => setOpen((prevOpen) => !prevOpen)}
-        >
-          <FontAwesomeIcon icon={open ? faCompressAlt : faExpandAlt} />
-        </button>
-      </div>
+    <div className={style.editorContainer}>
+      <div className={style.editorTitle}>{displayName}</div>
       <ControlledEditor
         onBeforeChange={handleChange}
         value={value}
-        className="code-mirror-wrapper"
+        className={style.codeMirrorWrapper}
         options={{
           lineWrapping: true,
           lint: true,
