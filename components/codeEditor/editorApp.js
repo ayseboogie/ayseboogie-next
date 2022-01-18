b9b3cf1e05145339b54f09c441b1a4583bfb7782
Ayse Stinnett <ayse.stinnett@gmail.com>
2022-01-17 21:24:56 -0500

    testing card in code editor on media queries

--- components/codeEditor/editorApp.js
+++ components/codeEditor/editorApp.js
@@ -21,90 +21,101 @@ const EditorApp = () => {
   const [css, setCss] = useLocalStorage(
     "css",
     `html {
-  height: 100%
-}
+    height: 100%
+  }
 
-body {
-  position: relative;
-  min-height: 100%;
-  background: #1F2937;
-}
-img {
-  vertical-align: middle;
-  max-width: 100%;
-}
+  body {
+    position: relative;
+    min-height: 100%;
+    background: #1F2937;
+  }
+  img {
+    max-width: 100%;
+  }
 
- /* Intro animation */
+  /* Intro animation */
 @keyframes intro {
-  from {
-    opacity: 0;
-    top: 0;
+    from {
+      opacity: 0;
+      top: 0;
+    }
+    to {
+      opacity: 1;
+      top: 50%;
+    }
   }
-  to {
-    opacity: 1;
-    top: 50%;
-  }
-}
 
 .container {
-  position: absolute;
-  width: 50%;
-  left: 50%;
-  vertical-align: center;
-  transform: translate(-50%,-70%);
-  animation: intro .7s cubic-bezier(0.175, 0.885, 0.32, 1.275) forwards;
-}
-@media (min-width: 1500px) {
-    .container {
-        transform: translate(-30%,-60%);
+    position: absolute;
+    width: 90%;
+    left: 50%;
+    vertical-align: center;
+    transform: translate(-50%,-60%);
+    animation: intro .7s cubic-bezier(0.175, 0.885, 0.32, 1.275) forwards;
+  }
+@media (min-width: 600px) {
+  .container {
+      width: 50%;
+      transform: translate(-50%,-90%);
     }
-}
+  }
+@media (min-width: 800px) {
+  .container {
+      width: 50%;
+      transform: translate(-50%,-70%);
+    }
+  }
+@media (min-width: 1500px) {
+  .container {
+      transform: translate(-30%,-70%);
+    }
+  }
 
 .card {
-  max-width: 400px;
-  position: relative;
-}
+    max-width: 400px;
+    position: relative;
+  }
 @media (min-width: 2000px) {
-    .card {
-  max-width: 640px;
-\t}
-}
+  .card {
+      max-width: 640px;
+    }
+  }
 .card--open .backside__form {
-  visibility: visible;
-  height: auto;
-  opacity: 1;
-  transform: translateY(6em);
-  padding-top: 10em;
-}
+    visibility: visible;
+    height: auto;
+    opacity: 1;
+    transform: translateY(6em);
+    padding-top: 10em;
+  }
 @media (min-width: 2000px) {
-    .card--open .backside__form {
-  transform: translateY(14em);
-  padding-top: 10em;
-}
-}
+  .card--open .backside__form {
+      transform: translateY(14em);
+      padding-top: 10em;
+    }
+  }
 
 .frontside___button {
-  position: absolute;
-  z-index: 1;
-  left: 50%;
-  transform: translateX(-50%);
-  overflow: hidden;
-  width: 100%;
-  display: block;
-  transition: transform .3s cubic-bezier(0.175, 0.885, 0.32, 1.275);
-}
+    position: absolute;
+    z-index: 1;
+    left: 50%;
+    transform: translateX(-50%);
+    overflow: hidden;
+    width: 100%;
+    display: block;
+    transition: transform .3s cubic-bezier(0.175, 0.885, 0.32, 1.275);
+  }
 
 .backside__form {
-  position: relative;
-  visibility: hidden;
-  width: 100%;
-  border-radius: .25em;
-  transition: 
-    opacity .4s ease-in-out,
-    height .4s ease-in-out,
-    transform .4s cubic-bezier(0.175, 0.885, 0.32, 1.275), 
-    padding .4s cubic-bezier(0.175, 0.885, 0.32, 1.275);
-}`
+    position: relative;
+    visibility: hidden;
+    width: 100%;
+    border-radius: .25em;
+    transition:
+        opacity .4s ease-in-out,
+        height .4s ease-in-out,
+        transform .4s cubic-bezier(0.175, 0.885, 0.32, 1.275),
+        padding .4s cubic-bezier(0.175, 0.885, 0.32, 1.275);
+  }`
   );
   //js
   const [js, setJs] = useLocalStorage(
@@ -156,7 +167,7 @@ img {
           onChange={setJs}
         />
       </div>
-      <div style={{ height: "60vh" }}>
+      <div style={{ height: "65vh" }}>
         <iframe
           srcDoc={srcDoc}
           title="output"
