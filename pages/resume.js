5f4313d7310f0828a60fe09c13f2349724a38060
Ayse Stinnett <ayse.stinnett@gmail.com>
2022-01-29 11:14:27 -0500

    lil less

--- pages/resume.js
+++ pages/resume.js
@@ -1,45 +1,38 @@
 import Link from "next/link";
-import {useState} from "react";
-import {Document, Page, pdfjs} from "react-pdf";
+import { useState } from "react";
+import { Document, Page, pdfjs } from "react-pdf";
 
-import {Viewer, Worker} from "@react-pdf-viewer/core";
-import {defaultLayoutPlugin} from "@react-pdf-viewer/default-layout";
-import "@react-pdf-viewer/core/lib/styles/index.css";
-import "@react-pdf-viewer/default-layout/lib/styles/index.css";
-import useWindowDimensions from "../utils/hooks/useWindowDimension.ts";
+import { Viewer, Worker } from "@react-pdf-viewer/core";
+import { defaultLayoutPlugin } from "@react-pdf-viewer/default-layout";
 
 pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.js`;
 
 function Resume() {
-    const [numPages, setNumPages] = useState(null);
-    const [pageNumber, setPageNumber] = useState(1);
+  const [numPages, setNumPages] = useState(null);
+  const [pageNumber, setPageNumber] = useState(1);
 
-    function onDocumentLoadSuccess({numPages}) {
-        setNumPages(numPages);
-    }
+  function onDocumentLoadSuccess({ numPages }) {
+    setNumPages(numPages);
+  }
 
-    const work = pdfjs.GlobalWorkerOptions.workerSrc;
-    const defaultLayoutPluginInstance = defaultLayoutPlugin();
-    const {windowWidth} = useWindowDimensions();
-
-    return (
-        <>
-            <div className="w-full md:pr-8 pt-6 md:bg-gray-800">
-                <div className="md:text-white tracking-widest flex justify-end">
-                    <Link href="/">back home</Link>
-                </div>
-            </div>
-            <div className="w-full md:flex items-center justify-center py-9">
-                <Document
-                    className="md:border-gray-800 md:border-2"
-                    file="/resume.pdf"
-                    onLoadSuccess={onDocumentLoadSuccess}
-                >
-                    <Page pageNumber={pageNumber}/>
-                </Document>
-            </div>
-        </>
-    );
+  return (
+    <>
+      <div className="w-full md:pr-8 pt-6 md:bg-gray-800">
+        <div className="md:text-white tracking-widest flex justify-end">
+          <Link href="/">back home</Link>
+        </div>
+      </div>
+      <div className="w-full md:flex items-center justify-center py-9">
+        <Document
+          className="md:border-gray-800 md:border-2"
+          file="/resume.pdf"
+          onLoadSuccess={onDocumentLoadSuccess}
+        >
+          <Page pageNumber={pageNumber} />
+        </Document>
+      </div>
+    </>
+  );
 }
 
 export default Resume;
