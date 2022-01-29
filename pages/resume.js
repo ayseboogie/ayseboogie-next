a5a14f9ea5217906e9ed61c79f1af4ddbb1b9cd8
Ayse Stinnett <ayse.stinnett@gmail.com>
2022-01-29 10:53:19 -0500

    changing workerUrl

--- pages/resume.js
+++ pages/resume.js
@@ -1,7 +1,7 @@
 import Link from "next/link";
-import { pdfjs } from "react-pdf";
-import { Viewer, Worker } from "@react-pdf-viewer/core";
-import { defaultLayoutPlugin } from "@react-pdf-viewer/default-layout";
+import {pdfjs} from "react-pdf";
+import {Viewer, Worker} from "@react-pdf-viewer/core";
+import {defaultLayoutPlugin} from "@react-pdf-viewer/default-layout";
 import "@react-pdf-viewer/core/lib/styles/index.css";
 import "@react-pdf-viewer/default-layout/lib/styles/index.css";
 import useWindowDimensions from "../utils/hooks/useWindowDimension.ts";
@@ -9,38 +9,40 @@ import useWindowDimensions from "../utils/hooks/useWindowDimension.ts";
 pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.js`;
 
 function Resume() {
-  const defaultLayoutPluginInstance = defaultLayoutPlugin();
-  const { windowWidth } = useWindowDimensions();
+    const work = pdfjs.GlobalWorkerOptions.workerSrc;
+    const defaultLayoutPluginInstance = defaultLayoutPlugin();
+    const {windowWidth} = useWindowDimensions();
 
-  return (
-    <>
-      <div className="w-full md:pr-8 pt-6 bg-gray-800">
-        <div className="text-white tracking-widest flex justify-end">
-          <Link href="/">back home</Link>
-        </div>
-      </div>
-      <div className="flex items-center justify-center">
-        {windowWidth < 640 && (
-          <Worker workerUrl="https://unpkg.com/pdfjs-dist@2.12.313/build/pdf.worker.js">
-            <div className="w-full md:w-3/4 py-6 ">
-              <Viewer fileUrl="/resume.pdf" defaultScale={0.6} />
+
+    return (
+        <>
+            <div className="w-full md:pr-8 pt-6 bg-gray-800">
+                <div className="text-white tracking-widest flex justify-end">
+                    <Link href="/">back home</Link>
+                </div>
             </div>
-          </Worker>
-        )}
-        {windowWidth > 640 && (
-          <Worker workerUrl="https://unpkg.com/pdfjs-dist@2.12.313/build/pdf.worker.js">
-            <div className="w-full md:w-3/4 py-6 ">
-              <Viewer
-                fileUrl="/resume.pdf"
-                plugins={[defaultLayoutPluginInstance]}
-                defaultScale={1}
-              />
+            <div className="flex items-center justify-center">
+                {windowWidth < 640 && (
+                    <Worker workerUrl={work}>
+                        <div className="w-full md:w-3/4 py-6 ">
+                            <Viewer fileUrl="/resume.pdf" defaultScale={0.6}/>
+                        </div>
+                    </Worker>
+                )}
+                {windowWidth > 640 && (
+                    <Worker workerUrl={work}>
+                        <div className="w-full md:w-3/4 py-6 ">
+                            <Viewer
+                                fileUrl="/resume.pdf"
+                                plugins={[defaultLayoutPluginInstance]}
+                                defaultScale={1}
+                            />
+                        </div>
+                    </Worker>
+                )}
             </div>
-          </Worker>
-        )}
-      </div>
-    </>
-  );
+        </>
+    );
 }
 
 export default Resume;
